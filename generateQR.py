68616036fb9095206ec3b250c3cb45b7a5713653
j-holou <holoubek.jan.spam@gmail.com>
2026-04-11 10:18:05 +0200

    RSVP z landing page nebude na mobilu

---
+++ generateQR.py
@@ -0,0 +1,18 @@
+#%%
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data('https://budemeholoubkovi.cz/')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.show()
+img.save("QRweb.png")
+
+# %%
