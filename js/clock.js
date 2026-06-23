64ce5ccaec3f15fc4a35ecf9a67ed493c068178c
j-holou <holoubek.jan.cz@gmail.com>
2026-06-23 22:14:10 +0200

    clock.js value fix; schedule modification

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2026-07-14 11:30", "Europe/Prague");
+  let targetDate = moment.tz("2026-07-18 11:30", "Europe/Prague");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
